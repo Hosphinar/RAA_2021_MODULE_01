4a5df5d8570ceff4a64957f12b0e5401eef3ea37
Hosphinar <110938417+Hosphinar@users.noreply.github.com>
2023-07-27 15:55:55 -0400

    Upload after IT changes.

--- RAA_2021_MODULE_01/Module01Challenge.cs
+++ RAA_2021_MODULE_01/Module01Challenge.cs
@@ -18,6 +18,7 @@ namespace RAA_2021_MODULE_01
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+            //Adding for a test
             // this is a variable for the Revit application
             UIApplication uiapp = commandData.Application;
 
